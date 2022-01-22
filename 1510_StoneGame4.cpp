595a0a201845661f788f8e8a5e1dabeca3af0980
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-01-22 23:04:25 +0530

    Create 1510_StoneGame4.cpp

---
+++ 1510_StoneGame4.cpp
@@ -0,0 +1,27 @@
+//Ques link- https://leetcode.com/problems/stone-game-iv/
+
+/*
+if next person looses, previous wins !
+
+TC O(N sqrt N)=N^(3/2)
+SC O(N)
+*/
+class Solution {
+public:
+    int dp[100001];
+    int helper(int n)
+    {
+        if(n<=0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        for(int i=1;i*i<=n;i++)
+        {
+            if(helper(n-(i*i))==0)
+                return dp[n]=1;
+        }
+        return dp[n]=0;
+    }
+    bool winnerSquareGame(int n) {
+        memset(dp,-1,sizeof dp);
+        return helper(n);
+    }
+};
